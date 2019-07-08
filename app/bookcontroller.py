0c1486e489d6702de4a9793f8fec55e234444fa4
Alessandro Dias <awilliansd@protonmail.com>
2019-07-08 10:17:42 -0400

    Inserção do swagger

--- app/bookcontroller.py
+++ app/bookcontroller.py
@@ -1,10 +1,11 @@
 from Service.bookservice import BookService
 from flask import Blueprint, jsonify
 from Domain.book import Book
+from flask_swagger import swagger
 
 bp_books = Blueprint('books', __name__)
 
 @bp_books.route('/getbooks', methods=['GET'])
 def GetBooks():
     result = BookService.GetBooks()
-    return jsonify(result), 200
+    return jsonify(swagger(result)), 200
